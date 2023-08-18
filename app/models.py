5b2f0741cb78533be9a67e57b8924ab7cdffe0d6
MulindwaMarkMyles <mylesmark753@gmail.com>
2023-08-18 12:54:30 +0300

    stage-1

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(null=True, max_length=300)
+    first_name = models.CharField(default="", null=False)
+    content = models.TextField(null=False)
+
